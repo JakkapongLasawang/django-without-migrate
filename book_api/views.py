b60895c11a67a8f6422c97ff5512f9922284f959
jakkaponglasawang <jakkaponglasawang@gmail.com>
2022-06-25 18:31:02 +0700

    format

--- book_api/views.py
+++ book_api/views.py
@@ -9,65 +9,76 @@ from mvc.error_msg import error_msg
 from mvc.response import js_response
 
 
+OK = "OK"
+NOT_JSON = "Invalid JSON"
+INPUT_INVALID = "Invalid input"
+CANNOT_CREATE = "Cannot Create"
+CANNOT_UPDATE = "Cannot Update"
+CANNOT_DELETE = "Cannot Delete"
+NOT_FOUND = "Not Found"
+
+
 class REST(View):
     def get(self, request):
         try:
             books = Book.objects.all().order_by('-published').values()
         except Book.DoesNotExist:
-            return js_response(400, "Not Found", {})
+            return js_response(400, "Not Found")
 
         response = parse_obj_as(List[GetBook], list(books))
-        return js_response(0, "OK", response)
+        return js_response(200, OK, response)
 
     def post(self, request):
         body = decode_payload(request.body)
         if body is None:
-            return js_response(400, "Invalid JSON", {})
+            return js_response(400, NOT_JSON)
 
         try:
             payload = CreateBook(**body).dict()
         except ValidationError as e:
-            return js_response(400, "Input not valid", error_msg(e))
+            return js_response(400, INPUT_INVALID, error_msg(e))
         book = None
         try:
             book = Book.objects.create(**payload)
         except Exception:
-            return js_response(400, "Cannot create book", {})
+            return js_response(400, CANNOT_CREATE)
 
-        return js_response(0, "success", GetBook(**book.__dict__))
+        return js_response(201, OK, GetBook(**book.__dict__))
 
     def put(self, request):
         body = decode_payload(request.body)
         if body is None:
-            return js_response(400, "Invalid JSON", {})
+            return js_response(400, NOT_JSON)
 
         try:
             payload = UpdateBook(**body).dict()
         except ValidationError as e:
-            return js_response(400, "Input not valid", error_msg(e))
+            return js_response(400, INPUT_INVALID, error_msg(e))
 
         book = None
         try:
             book = Book.objects.filter(id=payload['id'])
+            if not book:
+                return js_response(400, NOT_FOUND)
             del payload['id']
             book.update(**payload)
         except Exception:
-            return js_response(400, "Cannot update book", {})
+            return js_response(400, CANNOT_UPDATE)
 
-        return js_response(0, "success", GetBook(**book[0].__dict__))
+        return js_response(202, OK, GetBook(**book[0].__dict__))
 
     def delete(self, request):
         body = decode_payload(request.body)
         if body is None:
-            return js_response(400, "Invalid JSON", {})
+            return js_response(400, NOT_JSON)
 
         try:
             payload = DeleteBook(**body).dict()
         except ValidationError as e:
-            return js_response(400, "Input not valid", error_msg(e))
+            return js_response(400, INPUT_INVALID, error_msg(e))
         try:
             Book.objects.filter(id=payload['id']).delete()
         except Exception:
-            return js_response(400, "Cannot delete", {})
+            return js_response(400, CANNOT_DELETE)
 
-        return js_response(0, "success", {})
+        return js_response(204, OK)
--- mvc/response.py
+++ mvc/response.py
@@ -1,21 +1,19 @@
 #  Copyright © 2022., Brainergy Co., Ltd. All rights reserved.
-
 import json
-
 from django.http import HttpResponse
 
 
-def js_response(code, message, result):
-    jsr = JsonResponse(code, message, result)
-    response = HttpResponse(jsr.to_json())
+def js_response(code=200, msg=None, result={}):
+    jsr = JsonResponse(code, msg, result)
+    response = HttpResponse(jsr.to_json(), status=code)
     response["Content-Type"] = "application/json"
     return response
 
 
 class JsonResponse:
-    def __init__(self, code, message, result):
+    def __init__(self, code, msg, result):
         self.code = str(code)
-        self.message = str(message) if message is not None else None
+        self.msg = str(msg) if msg is not None else None
         self.result = result
 
     def to_json(self):
